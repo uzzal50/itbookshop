35547ac75497775d5add49241d8cebe4aa874f4b
ujol991 <85609516+ujol991@users.noreply.github.com>
2021-12-25 09:08:58 +0545

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home, Error, SingleBook, Search, Cart } from './pages'
+import { Container, Row } from 'react-bootstrap'
+import Navbar from './components/Navbar'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Navbar />
+        <Container>
+          <Routes>
+            <Route exact path='/' element={<Home />}></Route>
+            <Route exact path='/book/:id' element={<SingleBook />}></Route>
+            <Route exact path='/cart' element={<Cart />}></Route>
+            <Route exact path='/search/:query' element={<Search />}></Route>
+            <Route exact path='*' element={<Error />}></Route>
+          </Routes>
+        </Container>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
